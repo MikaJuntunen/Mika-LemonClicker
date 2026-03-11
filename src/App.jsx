2f88ab45c455324191c1944551992634ecff5aba
Mika Juntunen <p90387@edu.sasky.fi>
2026-03-11 14:27:41 +0200

    ensimmäinen commit

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+
+
+
+import Menu from './components/Menu'
+import { useState } from 'react'
+import Header from './components/Header'
+import Balance from './components/Balance'
+import Booster from './components/Booster'
+import Lemon from './components/Lemon'
+import './App.css'
+
+function App() {
+
+    // Luodaan tilamuuttuja, jossa tallennetaan napautusten määrä.
+  const [clicks, setClicks] = useState(0)
+    const handleClick = () => {
+    // Kasvatetaan napautusten määrää yhdellä.
+    setClicks(clicks + 1)
+  }
+
+
+
+  return (
+    <div className="root">
+      <div className="root_content">
+        <div className="container clicker">
+          <Header>lemon clicker</Header>
+          <Balance total={clicks} />
+          <Lemon onClick={handleClick} />
+          <Booster value="3.2" />
+        </div>
+      </div>
+      <Menu items={2} />
+
+    </div>  
+  )
+
+
+
+}
+
+export default App
---
+++ src/components/Lemon.jsx
@@ -0,0 +1,42 @@
+import lemon from '../assets/lemon-big.svg'
+
+function Lemon(props) {
+  return (
+    <div className="lemon">
+      <img src={lemon} alt="lemon" onClick={props.onClick} />
+    </div>
+  )
+}
+
+export default Lemon
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
