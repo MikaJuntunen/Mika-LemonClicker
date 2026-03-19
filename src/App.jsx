28dfcc691e6db080637457e1e81eeaa1c1834bef
MikaJuntunen <mika.juntunen1975@gmail.com>
2026-03-19 10:05:23 +0200

    lisää tuotteen ostokäsittelijän

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,26 @@ function App() {
     // Luodaan tilamuuttuja, johon tallennetaan pelin laskennalliset tiedot.
     const [stats, setStats] = useState({clicks: 0, balance: 0, increase: 1, itemstobuy: 0})
 
+      const handlePurchase = (id) => {
+    // Etsitään tunnistetta vastaavan tuotteen indeksi taulukosta.
+    const index = storeitems.findIndex(storeitem => storeitem.id == id)
+    // Varmistetaan, että käyttäjällä on varaa ostaa tuote.
+    if (stats.balance >= storeitems[index].price) {
+      // Tehdään kopiot tilamuuttujista.
+      let newstoreitems = [...storeitems]
+      let newstats = {...stats}
+      // Kasvatetaan tuotteiden määrää yhdellä.
+      newstoreitems[index].qty++
+      // Vähännetään varoista tuotteen hinta.
+      newstats.balance = newstats.balance - newstoreitems[index].price
+      // TODO Uusi tuotehinta
+      // Tallennetaan uudet tilamuuttujien arviot.
+      setStoreitems(newstoreitems)
+      setStats(newstats)
+    }
+  }
+
+
     const handleClick = () => {
     // Tehdään kopio stats-tilamuuttujasta.
     let newstats = {...stats}
@@ -31,11 +51,13 @@ function App() {
 
 
   return (
-    <AppRouter stats={stats} 
+         <AppRouter stats={stats} 
                storeitems={storeitems} 
-               handleClick={handleClick} />
+               handleClick={handleClick} 
+               handlePurchase={handlePurchase} />
+
   )
-  
+
 }
 
 export default App
